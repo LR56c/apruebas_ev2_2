b5d831085a28516b659941737aa031a8d8ffea7b
Mauricio Herrera Ortega <ronalli56@gmail.com>
2025-11-03 14:22:28 -0300

    force performance test fails

--- login-test.js
+++ login-test.js
@@ -4,8 +4,6 @@ import { check, sleep } from 'k6';
 export const options = {
     vus: 10,
     duration: '30s',
-    // vus: 5,
-    // duration: '10s',
     thresholds: {
         'http_req_duration': ['p(95)<800'],
         'http_req_failed': ['rate<0.01'],
