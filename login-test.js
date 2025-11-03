db2c78d57153632e733a593311e05c385bccec69
Mauricio Herrera Ortega <ronalli56@gmail.com>
2025-11-03 13:38:35 -0300

    update ci & reduce smoke test

--- login-test.js
+++ login-test.js
@@ -2,8 +2,10 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-    vus: 10,
-    duration: '30s',
+    // vus: 10,
+    // duration: '30s',
+    vus: 5,
+    duration: '10s',
     thresholds: {
         'http_req_duration': ['p(95)<800'],
         'http_req_failed': ['rate<0.01'],
