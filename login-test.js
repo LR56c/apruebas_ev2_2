c9f23732d5624e811c2cc37af44f5245267f6f59
Mauricio Herrera Ortega <ronalli56@gmail.com>
2025-11-03 14:54:34 -0300

    update ci

--- login-test.js
+++ login-test.js
@@ -3,7 +3,7 @@ import { check, sleep } from 'k6';
 
 export const options = {
     vus: 10,
-    duration: '30s',
+    duration: '10s',
     thresholds: {
         'http_req_duration': ['p(95)<800'],
         'http_req_failed': ['rate<0.01'],
